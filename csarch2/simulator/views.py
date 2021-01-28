85e2b1e06781e0257b0b311c8a341979b5cb8a3c
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2021-01-28 23:11:44 +0800

    output after performing operations is now displayed

--- csarch2/simulator/views.py
+++ csarch2/simulator/views.py
@@ -7,7 +7,7 @@ save = [] #holder for download
 
 # Create your views here.
 def index(request):
-  if request.method == "POST":
+  """ if request.method == "POST":
     #compute
     if request.POST.get('cache_access_time'):
       global save
@@ -44,12 +44,40 @@ def index(request):
       file_data = save
       response = HttpResponse(file_data, content_type='application/text charset=utf-8')
       response['Content-Disposition'] = 'attachment; filename="BSA_LRU_answer.txt"'
-      return response
+      return response """
   return render(request, 'pages/home.html')
 
 def perform_bsa_lru(request):
-  sample_data = {
-    'hello': 'world'
-  }
-  return JsonResponse(sample_data)
+  t_cache          = int(request.POST.get('cache_access_time'))
+  t_mem            = int(request.POST.get('memory_access_time'))
+  sets             = int(request.POST.get('sets'))
+  n_blocks_per_set = int(request.POST.get('blocks_per_set'))
+  words_per_block  = int(request.POST.get('words_per_block'))
+  num_list         = request.POST.get('input').splitlines()
+  num_list         = [int(i) for i in num_list] #convert all to int
+  iterations       = int(request.POST.get('iteration'))
+  bs = BlockSetAssociative(
+    n_sets=sets, 
+    n_blocks=n_blocks_per_set, 
+    num_list=num_list, 
+    n_words=words_per_block,
+    t_cache=t_cache,
+    t_mem=t_mem,
+    debug=False)
   
+  for i in range(iterations):
+    bs.hits = 0
+    bs.misses = 0
+    bs.lru()
+
+  final_result = {
+    'total_time_lt': bs.get_total_time_lt(),
+    'total_time_nlt': bs.get_total_time_nlt(),
+    'avg_time': bs.get_avg_time(),
+    'hits': bs.hits,
+    'misses': bs.misses,
+    'hit_rate': bs.get_hit_rate(),
+    'miss_penalty': bs.get_miss_penalty()
+  }
+
+  return JsonResponse(final_result)
